b86bd98992645e2932c08d5ef3efd8b74da298e4
Miguel Rafael <52474593+miguel007TMM2@users.noreply.github.com>
2021-01-30 10:37:07 -0400

    cration hours added

--- backend/src/models/comments.js
+++ backend/src/models/comments.js
@@ -6,7 +6,8 @@ let idUnique = uuidv4();
 const commentSchema = new Schema({
    publication_id:{type: String, required: true},
    user_id:{type: String, required: true},
-   descripcion:{type: String, default: idUnique}
+   descripcion:{type: String, default: idUnique},
+   createdAt:{ type: Date, default: Date.now}
 })
 
 module.exports = model('Comment', commentSchema)
--- backend/src/models/publication.js
+++ backend/src/models/publication.js
@@ -7,7 +7,8 @@ const publicationSchema = new Schema({
    description:{type: String},
    image:{type: String, default:idUnique},
    user_id:{type: String, required: true},
-   likes:{type: Number, required: true}
+   likes:{type: Number, required: true},
+   createdAt:{ type: Date, default: Date.now}
 })
 
 module.exports = model('Publication', publicationSchema)
