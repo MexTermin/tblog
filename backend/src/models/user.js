dd45e3bb8be070589305a83f1721a9e8ba267098
Miguel Rafael <52474593+miguel007TMM2@users.noreply.github.com>
2021-02-04 00:26:00 -0400

    New method to validate info user

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -3,8 +3,7 @@ const bcrypt = require('bcrypt-node')
 const {v4: uuidv4 } = require('uuid')
 
 const userSchema = new Schema({
-    name:{ type: String, required: true},
-    lastName:{ type: String, required: true},
+    username: {type: String, required: true},
     email:{ type: String, required: true},
     password:{ type: String, required: true},
     description:{type: JSON},
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,17 +2,19 @@ const {Router} = require("express");
 const bcrypt = require('bcrypt-node')
 const router = Router();
 const users = require('../models/user');
+const bodyParser = require('body-parser')
+const { check, validationResult } = require('express-validator');
 
+const urlencodeParser = bodyParser.urlencoded({extended: false})
 
 const getUserinfo = async(res, req) => { 
     
     const email = req.body.email;
 
     const emailFind = await users.findOne({"email": email})
-    if(emailFind == null && req.body.password){
+    if(emailFind == null){
         const newUser = new users({
-            name : req.body.name , 
-            lastName:req.body.lastName,     
+            username: req.body.username,       
             email:req.body.email, 
             password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)), 
             description:req.body.description,
@@ -20,7 +22,7 @@ const getUserinfo = async(res, req) => {
         })
         return newUser
     }
-    console.log('Se encuentra en la base de datos o no ha escrito la contraseña')
+    console.log('Se encuentra en la base de datos')
     res.redirect('/signin')
  }
 
@@ -40,14 +42,30 @@ router.get('/signin', (res, req)=>{
         text:'signin'
     })
 })
-router.post('/signin', async (req, res)=>{
+router.post('/signin',urlencodeParser,[
+    check('username', 'This username must be 3+ characters long')
+        .exists()
+        .isLength({min: 3}),
+    check('email', 'Email is not valid')
+        .isEmail()
+        .normalizeEmail(),
+    check('password', 'Password should be at least 5 chars long or is not alphanumeric')
+        .isLength({min: 5})
+        .isAlphanumeric()
+],(req, res)=>{
+        const error = validationResult(req)
+        if(!error.isEmpty()){
+            return res.status(422).jsonp(error.array())
+        }
         getUserinfo(res,req)
             .then(function(result, reject){
                 if(result){
                     result.save()
                     res.redirect('/login')
                 }
+                res.redirect('/signin')
             })
+            .catch(error => console.log(error))
 })
 
 
