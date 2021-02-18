22a4a0bd77e16f5484865237f90f7727d508d30a
Miguel Rafael <52474593+miguel007TMM2@users.noreply.github.com>
2021-02-18 11:37:43 -0400

    new route was added in the path get_user

--- backend/src/routes/get_user.js
+++ backend/src/routes/get_user.js
@@ -2,8 +2,18 @@ const {Router} = require("express");
 const router = Router();
 const User = require('../models/user');
 
-router.get('/:id', (req, res)=>{
-    User.findById(req.params.id)
+router.get('/all', async (req, res)=>{
+    await User.find()
+        .then(user=>{
+            return res.json(user)
+        })
+        .catch((e)=>{
+            return e
+        })
+})
+
+router.get('/:id', async (req, res)=>{
+    await User.findById(req.params.id)
         .then(user =>{
             const notFound = {
                 message:"User is not in database maybe the id that you give is incorrect."
@@ -19,4 +29,27 @@ router.get('/:id', (req, res)=>{
         })
 })
 
+router.get('/:email', async (req, res)=>{
+   await User.findById(req.params.email)
+    .then(user =>{
+        const notFound = {
+            message:"User isn't found in database maybe the email that you give is incorrect."
+        }
+        if(user){
+            return res.json(user)
+        }
+        return res.send(notFound)
+    })
+    .catch((err) => {
+        console.log(err)
+        return res.json(err)
+    })
+})
+
+router.delete('/:id', async (req, res)=>{
+    await User.findByIdAndDelete(req.params.id)
+    res.json({
+        text: 'User was delecte correctly'
+    })
+})
 module.exports = router;
