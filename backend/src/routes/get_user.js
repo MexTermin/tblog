85e5842cc4f29f20928d201c1009ea49134f7de7
yael <yaeldemil01@hotmail.com>
2021-03-02 23:35:42 -0400

    resolved incorrect response message

--- backend/src/routes/get_user.js
+++ backend/src/routes/get_user.js
@@ -1,7 +1,10 @@
 const {Router} = require("express");
 const router = Router();
 const User = require('../models/user');
-
+const { check } = require("express-validator");
+const bodyParser = require("body-parser");
+const { validationResult } = require('express-validator');
+const urlencodeParser = bodyParser.urlencoded({ extended: false });
 router.get('/all', async (req, res)=>{
     await User.find()
         .then(user=>{
@@ -12,21 +15,33 @@ router.get('/all', async (req, res)=>{
         })
 })
 
-router.get('/:id', async (req, res)=>{
-    await User.findById(req.params.id)
-        .then(user =>{
-            const notFound = {
-                message:"User is not in database maybe the id that you give is incorrect."
-            }
-            if(user){
-                return res.json(user)
-            }
-            return res.send(notFound)
-        })
-        .catch((err) => {
-            console.log(err)
-            return res.json(err)
-        })
+router.get('/:id',
+    urlencodeParser,
+    [
+    check("id","the field likes must be a valid number")
+    .exists()
+    .isLength({ min: 24 })
+    ],
+    async (req, res)=>{
+        const error = validationResult(req)
+        if (!error.isEmpty()) {
+        
+            return res.json({'message':error.array()[0].msg})
+        }
+        await User.findById(req.params.id)
+            .then(user =>{
+                const notFound = {
+                    message:"User is not in database maybe the id that you give is incorrect."
+                }
+                if(user){
+                    return res.json(user)
+                }
+                return res.send(notFound)
+            })
+            .catch((err) => {
+                console.log(err.message)
+                return res.json(err.message)
+            })
 })
 
 router.get('/:email', async (req, res)=>{
